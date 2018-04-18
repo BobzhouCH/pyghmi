a2d3820a5769b8a84be568f25258725a1f0c9320
Bobzhou <zhoub1986@aliyun.com>
2018-04-18 20:32:46 +0800

    update performance of IPMI get opeartion for huawei server

--- pyghmi/ipmi/oem/huawei/snmp/fru.py
+++ pyghmi/ipmi/oem/huawei/snmp/fru.py
@@ -14,22 +14,22 @@ class FRU(object):
     def get_info(self):
         ###### Chassiss info######
         inf = self.info
-        fru_output = self.ipmi_session.run_command('fru list')
-        inf['Chassis type'] = fru_output[1].split(':')[1].strip()
+        fru_output = self.ipmi_session.run_command('fru list 0')
+        inf['Chassis type'] = fru_output[0].split(':')[1].strip()
         inf['Chassis part number'] = ''
         inf['Chassis serial number'] = ''
 
         ###### Board info######
         inf['Board manufacture date'] = ''
-        inf['Board manufacturer'] = fru_output[3].split(':')[1].strip()
-        inf['Board product name'] = fru_output[4].split(':')[1].strip()
-        inf['Board serial number'] = fru_output[5].split(':')[1].strip()
+        inf['Board manufacturer'] = fru_output[2].split(':')[1].strip()
+        inf['Board product name'] = fru_output[3].split(':')[1].strip()
+        inf['Board serial number'] = fru_output[4].split(':')[1].strip()
         inf['Board model'] = ''
 
         ###### Manufacturer info######
-        inf['Manufacturer'] = fru_output[15].split(':')[1].strip()
-        inf['Product name'] = fru_output[16].split(':')[1].strip()
+        inf['Manufacturer'] = fru_output[14].split(':')[1].strip()
+        inf['Product name'] = fru_output[15].split(':')[1].strip()
         inf['Model']  = ''
         inf['Hardware Version'] = ''
-        inf['Serial Number'] = fru_output[18].split(':')[1].strip()
+        inf['Serial Number'] = fru_output[17].split(':')[1].strip()
         inf['Asset Number'] = ''
